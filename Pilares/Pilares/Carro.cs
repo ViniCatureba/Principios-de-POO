5bf1975f74e1f824f9c51bdcf89c04ef25526235
Vinicius Catureba <vinicius.bigao14@gmail.com>
2025-03-26 21:46:32 -0300

    Pilares de POO

---
+++ Pilares/Pilares/Carro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilares
+{
+    class Carro
+    {
+        //public / private / protected / internal (encapsulamento do c#)
+        public string marca;
+        private string _modelo;
+        public int anoFabricacao;
+
+
+        //Get e Set
+
+
+        //metodos
+
+        public void SetModelo(string mod)
+        {
+            if(mod=="Fiesta" || mod == "HB20")
+            {
+                _modelo = mod;
+            }
+            else
+            {
+                Console.WriteLine("Alteracao invalida");
+            }
+        }
+        public string GetModelo()
+        {
+            return _modelo;
+        }
+    }
+}
---
+++ Pilares/Pilares/Program.cs
@@ -0,0 +1,26 @@
+﻿//Pilares de POO
+
+//Ferramentas - Formas de Trabalhar com POO
+
+//Encapsulamento e Herença
+
+//Abstração e Polimorfismo
+
+//Encapsulamento - Esconder comportamentos e atributos
+
+//public / private / protected / internal (encapsulamento do c#)
+
+// public - Ao usar em atributos, metodos ou classe pode ser acessado por qualquer um
+
+// Private -Esse atributo/metodo/classe só pode ser acessado dentro da propria classe
+// Vantagens: 1º Esconder atribulots/metodos/classes; Defino as regras para alteracao ou listagem
+
+// protected/internal - Esse atributo/metodo/clase so pode ser acesso pela classe criada ou pela classe pai
+
+
+
+//Get -> Metodo para pegar um atributo privato
+
+//Set -> metodo para alterar atributo privado
+
+
